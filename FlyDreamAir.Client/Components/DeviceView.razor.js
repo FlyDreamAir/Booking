558f42322bba5d20707f5c088d189844abea2174
Trung <57174311+trungnt2910@users.noreply.github.com>
2024-04-13 17:13:30 +1000

    chore(ui): Client WebAssembly work
    - Set up MudBlazor for the WebAssembly client project.
    - Add a method to detect the current device using the client's
    JavaScript runtime to conditionally render some elements.
    - Move the Home page from server rendered to client rendered.
    
    MainLayout still needs to be rendered by the server since
    some pages (especially those generated by ASP.NET Core Identity)
    need to be server-rendered.

---
+++ FlyDreamAir.Client/Components/DeviceView.razor.js
@@ -0,0 +1,31 @@
+﻿window.DeviceView = (function () {
+    let matchMedia = window.matchMedia("only screen and (max-width: 768px)");
+    let components = new Set();
+
+    matchMedia.addEventListener("change", function () {
+        components.forEach(function (c) {
+            c.invokeMethodAsync("OnChange", matchMedia.matches);
+        });
+    });
+
+    async function registerAsync(c) {
+        if (!components.has(c)) {
+            components.add(c);
+            await c.invokeMethodAsync("OnChange", matchMedia.matches);
+        }
+    }
+
+    function unregister(c) {
+        components.delete(c);
+    }
+
+    function isMobile() {
+        return matchMedia.matches;
+    }
+
+    return {
+        RegisterAsync: registerAsync,
+        Unregister: unregister,
+        IsMobile: isMobile
+    }
+})();
--- FlyDreamAir.Client/Program.cs
+++ FlyDreamAir.Client/Program.cs
@@ -1,11 +1,14 @@
 using FlyDreamAir.Client;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
