1ebf5d4888885efe092cd28db7a93136c01a960d
Trung <57174311+trungnt2910@users.noreply.github.com>
2024-04-23 01:21:10 +1000

    refactor(ui): Refactor Seats page
    - Split large seats view portion into its own component.
    - Split most of the page logic into `AddOnPageBase`, which will be
    used by the `AddOns` page in the future.
    - Track the flight index and amount of all `AddOn`s.

---
+++ FlyDreamAir.Data/Model/PreOrderedAddOn.cs
@@ -0,0 +1,8 @@
+﻿namespace FlyDreamAir.Data.Model;
+
+public class PreOrderedAddOn
+{
+    public required AddOn AddOn { get; set; }
+    public required Flight Flight { get; set; }
+    public decimal Amount { get; set; }
+}
