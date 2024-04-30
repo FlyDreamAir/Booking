34ab11b9cf20e37a92cba515bae63c060a8bc6f6
Trung <57174311+trungnt2910@users.noreply.github.com>
2024-04-30 16:16:50 +1000

    feat(ui): Add Explore page
    This is the last page included in FlyDreamAir's basic design.
    This commit also concludes Milestone 2.

---
+++ FlyDreamAir.Client/Services/NewsService.cs
@@ -0,0 +1,16 @@
+﻿using FlyDreamAir.Data.Model;
+
+namespace FlyDreamAir.Client.Services;
+
+public class NewsService : HttpApiService
+{
+    public NewsService(HttpClient httpClient)
+        : base(httpClient)
+    {
+    }
+
+    public IAsyncEnumerable<Post> GetPostsAsync()
+    {
+        return _GetObjectsFromJsonAsAsyncEnumerable<Post>();
+    }
+}
