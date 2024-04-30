42cb69ef9b2485b1338a3513dd298032857320c5
Trung <57174311+trungnt2910@users.noreply.github.com>
2024-04-30 10:06:58 +1000

    refactor: News API refactor
    - Use the correct coding style.
    - Rename `News` class to `Post`.
    - Use more appropriate data types for some fields in `Post`.

--- FlyDreamAir/Controllers/NewsController.cs
+++ FlyDreamAir/Controllers/NewsController.cs
@@ -1,25 +1,22 @@
-using FlyDreamAir.Data;
 using FlyDreamAir.Data.Model;
 using FlyDreamAir.Services;
-using FlyDreamAir.Utils;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Globalization;
-using System.Security.Claims;
 
 namespace FlyDreamAir.Controllers;
 
-[Route("api/News")]
-public class NewsController: ControllerBase {
-	private readonly NewsService _newsService;
-	
-	public NewsController(NewsService ns) {
-		_newsService = ns;
-	}
-	
-	[HttpGet(nameof(GetNews))]
-	public ActionResult<IAsyncEnumerable<News>> GetNews() {
-		return Ok(_newsService.GetNewsAsync());
-	}
+[Route("api/[controller]")]
+public class NewsController : ControllerBase
+{
+    private readonly NewsService _newsService;
+
+    public NewsController(NewsService ns)
+    {
+        _newsService = ns;
+    }
+
+    [HttpGet(nameof(GetPosts))]
+    public ActionResult<IAsyncEnumerable<Post>> GetPosts()
+    {
+        return Ok(_newsService.GetPosts());
+    }
 }
