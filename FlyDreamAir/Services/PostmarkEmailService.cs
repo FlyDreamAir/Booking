485f60739d3dc89713c364edd9755450f0a1519f
Trung <57174311+trungnt2910@users.noreply.github.com>
2024-05-11 19:56:25 +1000

    feat: Add professional email template

--- FlyDreamAir/Services/PostmarkEmailService.cs
+++ FlyDreamAir/Services/PostmarkEmailService.cs
@@ -6,12 +6,34 @@ public class PostmarkEmailService : IEmailService
 {
     private readonly PostmarkClient _client;
     private readonly string _adminEmail;
+    private readonly string _template;
 
-    public PostmarkEmailService(IConfiguration configuration, PostmarkClient client)
+    public PostmarkEmailService(
+        IConfiguration configuration,
+        IWebHostEnvironment webHostEnvironment,
+        PostmarkClient client
+    )
     {
         _client = client;
-        _adminEmail = configuration["Admin:Email"]
+        _adminEmail = configuration["Admin:Email:Address"]
             ?? "FlyDreamAir Administrator <admin@fly.trungnt2910.com>";
+        var templateConfig = configuration["Admin:Email:Template"];
+        if (!string.IsNullOrEmpty(templateConfig))
+        {
+            var templatePath = Path.Combine(webHostEnvironment.WebRootPath, templateConfig);
+            if (File.Exists(templatePath))
+            {
+                _template = File.ReadAllText(templatePath);
+            }
+            else
+            {
+                _template = templateConfig;
+            }
+        }
+        else
+        {
+            _template = "{{Body}}";
+        }
     }
 
     public async Task SendEmailAsync(string to, string subject, string body, string htmlBody)
@@ -24,7 +46,9 @@ public class PostmarkEmailService : IEmailService
                 TrackOpens = true,
                 Subject = subject,
                 TextBody = body,
-                HtmlBody = htmlBody
+                HtmlBody = _template
+                    .Replace("{{Subject}}", subject)
+                    .Replace("{{Body}}", htmlBody)
             }
         );
     }
